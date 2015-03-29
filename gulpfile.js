53b15a0fdc5cf2c6a4a5f26c95a951d4f9e001ff
Rodion Aznaurov <radik_az@mail.ru>
2015-03-29 20:38:54 +0300

    Title ch.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', function() {
+gulp.task('sass', function() {
   gulp.src('./scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./css'))
 });
 
+gulp.task('watch', function() {
+  gulp.watch('./scss/**/*.scss', ['default']);
+});
+
+gulp.task('default', ['sass', 'watch']);
